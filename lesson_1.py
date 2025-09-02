33c5eee1996bc4172a2fa6222211efc69f782be9
Nurkanat-Kaliev <kanur0563@gmail.com>
2025-09-03 00:22:31 +0600

    lesson_1

--- lesson_1.py
+++ lesson_1.py
@@ -6,10 +6,15 @@ class Car:
 
     def driving_at(self,speed):
         print(f"{self.model} is driving at speed {speed}")
+
+    def driving_with(self,car):
+        print(f"Drivivng with {car}")
 car_honda = Car("silver","honda")
+
 print(car_honda.color)
 
 car_bmw = Car("black","bmw")
 car_bmw.driving_at(67)
+car_bmw.driving_with("mercedes")
 car_bmw.color = "white"
 print(car_bmw.color,car_bmw.model)

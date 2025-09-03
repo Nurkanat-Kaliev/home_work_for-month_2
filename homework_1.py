6992ac4289f6bf169c1eab810286063d067fedb8
Nurkanat-Kaliev <kanur0563@gmail.com>
2025-09-03 17:29:55 +0600

    hw1

--- homework_1.py
+++ homework_1.py
@@ -11,7 +11,7 @@ class Person:
               f"birth date: {self.birth_date}\n"
               f"occupation: {self.occupation}\n"
               f"higher education: {self.higher_education}\n")
-888888
+
 identity = Person("Soup","08.09.1986","SAS soldier",False)
 identity_2 = Person("Price","06.11.1977","SAS soldier",False)
 identity_3 = Person("Trump","14.07.1946","US presidnet",True)

e36cb327bae37a3add19f1859e384761e9e9744b
Nurkanat-Kaliev <kanur0563@gmail.com>
2025-09-21 11:22:31 +0600

    added hw5

--- homeworks/homework_5.py
+++ homeworks/homework_5.py
@@ -1,29 +1,66 @@
-def check_admin(status):
-    def functions(func):
+# def check_admin(status):
+#     def decorator(func):
+#         def wrapper(*args):
+#             balance = 0
+#             if status == "admin":
+#                 print(f"Access allowed for {status}")
+#                 balance += 500
+#                 print(f"Balance filled on {balance}$")
+#                 return func(*args)
+#
+#
+#             else:
+#                 print(f"Access denied for {status}")
+#                 print("balance remained unchanged")
+#
+#         return wrapper
+#     return decorator
+#
+# @check_admin("arzy")
+# def alien():
+#     print("the user is banned")
+#
+# @check_admin("admin")
+# def welcome():
+#     print("Got the access")
+#
+# alien()
+# welcome()
+#
+#
+
+
+
+
+class User:
+    def __init__(self,role,login):
+        self.role = role
+        self.login = login
+
+admin = User("admin", "Kanat")
+alien = User("Arzu", "Obanai")
+
+def check_admin(obj_user):
+    def decorator(func):
         def wrapper(*args):
-            balance = 0
-            if status == "admin":
-                print(f"Access allowed for {status}")
-                balance += 500
-                print(f"Balance filled on {balance}$")
+                if obj_user.role == "admin":
+                    print(f"Access allowed for {obj_user.login}")
+                    return func(*args)
+                else:
+                    print(f"access denied for {obj_user.login}")
 
-            else:
-                print(f"Access denied for {status}")
-                print("balance remained unchanged")
-
-            return func(*args)
         return wrapper
-    return functions
+    return decorator
 
-@check_admin("arzy")
-def alien():
-    print("the user is banned")
+@check_admin(admin)
+def allowed():
+    print("U can go")
 
-@check_admin("admin")
-def welcome():
-    print("Got the access")
+@check_admin(alien)
+def not_allowed():
+    print("U cant go")
 
-alien()
-welcome()
+allowed()
+not_allowed()
 
 
